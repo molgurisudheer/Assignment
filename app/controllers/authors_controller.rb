003c002f8fae01f6b90ead23236ac410135b4a13
sudheer <molguri.sudheer@gmail.com>
2020-10-05 07:29:37 +0530

    First commit

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,21 @@
+class AuthorsController < ApplicationController
+  def index
+  	 @authors = Author.all
+  end
+
+  def show
+   @author = Author.find(params[:id])	
+  end
+
+  def new
+  end
+
+  def edit
+  @author = Author.find(params[:id])
+
+  end
+
+def update
+fgfdgfdgfdg
+end
+end
